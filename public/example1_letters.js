d6d110dd2c9a83ef21f7381eb46c0bbe8ee89b26
Yuriy Dupyn <yuriy.dupyn@gmail.com>
2020-10-03 11:32:57 +0200

    Implement a parser that succeeds if it sees a 'c' character as the first token

---
+++ public/example1_letters.js
@@ -0,0 +1,31 @@
+
+import { Parser, ParserValue } from "../src/index";
+
+// ===== HELPERS =====
+// String -> Bool
+function isEmptyString(s) {
+  return s.length == 0;
+}
+
+// === LETTER ===
+// Character -> Boolean
+function isChar(d) {
+  // TODO
+
+}
+
+// TODO: lowercase, uppercase, case-insensitive
+
+// Parser(Char)
+export const cCharacter = Parser(s => {
+  if (isEmptyString(s)) {
+    return ParserValue.failure("Expected a 'c' character instead of empty string");
+  } else if (s[0] == "c"){
+    return ParserValue.success({val: "c", rest: s.slice(1)});
+  } else {
+    return ParserValue.failure(`Expected a 'c' character instead of '${s[0]}'`);
+  }
+});
+
+
+
